3b96f5f3507fb07bb3e18f70e5780bd289b9a3ec
Harry <henriquecbgamer@gmail.com>
2024-07-10 20:37:05 -0300

    Update all ExpressFiles

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const fs = require('fs');
 
 const app = express();
 
+const possibleExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.txt', '.pdf'];
+
 // Função para criar recursivamente diretórios
 const criarDiretoriosRecursivamente = (diretorio) => {
   if (!diretorio) return; // Adiciona verificação para diretório vazio
   const partes = diretorio.split('/');
   fs.mkdirSync(diretorio, { recursive: true });
-
 };
 
 // Configuração do Multer para armazenar imagens
@@ -46,46 +47,127 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Rota para lidar com o envio de imagens e texto
-app.post('/upload', upload.single('imagem'), (req, res) => {
-  const { name, folders } = req.body;
-  if (!folders) {
-    return res.status(400).json({ error: 'O campo "folders" é obrigatório.' });
+// Função para mover e substituir arquivos independentemente da extensão
+const moverArquivo = (tempFilePath, finalDir, name, res) => {
+  const newFileExtension = path.extname(tempFilePath);
+  const finalFilePath = path.join(finalDir, `${name}${newFileExtension}`);
+
+  // Remover arquivos antigos
+  for (const ext of possibleExtensions) {
+    const oldFilePath = path.join(finalDir, `${name}${ext}`);
+    if (fs.existsSync(oldFilePath)) {
+      try {
+        fs.unlinkSync(oldFilePath); // Remove o arquivo antigo, independentemente da extensão
+      } catch (err) {
+        console.error(`Erro ao remover o arquivo antigo: ${oldFilePath}`, err);
+      }
+    }
   }
 
-  const tempFilePath = path.join(__dirname, 'public/temp', req.file.originalname);
-  const finalDir = path.join(__dirname, 'public', folders);
-  const finalFilePath = path.join(finalDir, `${name}${path.extname(req.file.originalname)}`);
-
-  // Cria recursivamente o diretório final se não existir
+  // Cria o diretório se não existir
   criarDiretoriosRecursivamente(finalDir);
 
-  // Move o arquivo do diretório temporário para o diretório final
+  // Mover o novo arquivo para o destino
   fs.rename(tempFilePath, finalFilePath, (err) => {
     if (err) {
-      return res.status(500).json({ error: 'Erro ao mover o arquivo.' });
+      return res.status(500).json({ error: 'Erro ao mover o arquivo.' + err });
     }
     res.json({
       message: 'Upload realizado com sucesso!',
-      file: finalFilePath,
-      body: req.body
+      file: finalFilePath
     });
   });
+};
+
+const obterDiretorioDestino = (tipo, id) => {
+  switch (tipo) {
+    case 'user':
+      return `public/user/${id}`;
+    case 'assets':
+      return 'public/assets';
+    case 'novel':
+      return `public/novel/${id}`;
+    case 'novel-assets':
+      return `public/novel/${id}/assets`;
+    default:
+      throw new Error('Tipo de upload inválido.');
+  }
+};
+
+
+// Rota para lidar com o envio de imagens e texto
+app.post('/upload', upload.single('imagem'), (req, res) => {
+  const { name, tipo, id } = req.body;
+
+  // Verificação do campo "tipo"
+  if (!tipo) {
+    return res.status(400).json({ error: 'O campo "tipo" é obrigatório.' });
+  }
+
+  // Verificação do campo "name"
+  if (!name) {
+    return res.status(400).json({ error: 'O campo "name" é obrigatório.' });
+  }
+
+  // Verificação do arquivo de upload
+  if (!req.file) {
+    return res.status(400).json({ error: 'O campo "imagem" é obrigatório.' });
+  }
+
+  let finalDir;
+  try {
+    finalDir = path.join(__dirname, obterDiretorioDestino(tipo, id));
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  const tempFilePath = path.join(__dirname, 'public/temp', req.file.originalname);
+
+  moverArquivo(tempFilePath, finalDir, name, res);
 });
 
-app.get('/image/:name', (req, res) => {
+// Função para servir imagens ou arquivos
+const servirArquivos = (directory, req, res) => {
   const name = req.params.name;
-  const directoryPath = path.join(__dirname, 'public/image');
-  const possibleExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg']; // Adicione mais extensões conforme necessário
 
   for (const ext of possibleExtensions) {
-    const filePath = path.join(directoryPath, `${name}${ext}`);
+    const filePath = path.join(directory, `${name}${ext}`);
     if (fs.existsSync(filePath)) {
       return res.sendFile(filePath);
     }
   }
 
-  res.status(404).send('Imagem não encontrada');
+  res.status(404).send('Arquivo não encontrado');
+};
+
+// Rota para imagens
+app.get('/image/:name', (req, res) => {
+  const directoryPath = path.join(__dirname, 'public/image');
+  servirArquivos(directoryPath, req, res);
+});
+
+// Rota para user profile
+app.get('/user/:id/:name', (req, res) => {
+  const directoryPath = path.join(__dirname, `public/user/${req.params.id}`);
+  servirArquivos(directoryPath, req, res);
+});
+
+// Rota para assets
+app.get('/assets/:name', (req, res) => {
+  const directoryPath = path.join(__dirname, 'public/assets');
+  servirArquivos(directoryPath, req, res);
+});
+
+// Rota para novel
+app.get('/novel/:id/:name', (req, res) => {
+  const directoryPath = path.join(__dirname, `public/novel/${req.params.id}`);
+  servirArquivos(directoryPath, req, res);
+});
+
+// Rota para assets de novel
+app.get('/novel/:id/assets/:name', (req, res) => {
+  const directoryPath = path.join(__dirname, `public/novel/${req.params.id}/assets`);
+  servirArquivos(directoryPath, req, res);
 });
 
 // Inicia o servidor
